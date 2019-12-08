1a324ff3f055c31bd2d2af8d943d61e5a3eae523
Shuchita <s.r.shuchi@gmail.com>
2019-12-08 10:30:13 +0600

    Create Valid Parentheses Solution.py

---
+++ Valid Parentheses Solution.py
@@ -0,0 +1,17 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+                                                    # Critical tesCase ")" "}}" 
+       queue =[]                                    # suppose s = "{}(}"  
+        for i in s:
+                if i != ")"and i != "}" and i !="]": 
+                     queue.append(i)
+                else:
+                    if len(queue) == 0:
+                        return False
+                    check = queue.pop()
+                    if i == ')' and check !='(' or i == '}' and check !='{' or i == ']' and check !='[':
+                        return False
+
+        if len(queue) == 0:
+                return True
+                
