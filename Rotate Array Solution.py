9d875216caea9027ac60e0851a4fba516abf1da9
Shuchita <s.r.shuchi@gmail.com>
2019-12-31 12:42:28 +0600

    Update Rotate Array Solution.py

--- Rotate Array Solution.py
+++ Rotate Array Solution.py
@@ -1,7 +1,7 @@
 class Solution:
-    def rotate(self, nums: List[int], k: int):
-     
-        k= k % len(nums)
+    def rotate(self, nums: List[int], k: int):           #Critical test Case k=3 nums=[1,2]
+                                                 
+        k= k % len(nums)                                 #and test case k= 0 nums = anything   
         if k == 0:
             return
         nums[:]= nums[-k:]+nums[:-k]
